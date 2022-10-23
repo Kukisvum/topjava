c0f53984da360ca95f241b4b8f36d2af2d21531b
Valeriy Kashin <valera-519@mail.ru>
2022-10-23 21:19:45 +0300

    HW 03 base

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,54 @@
+package ru.javawebinar.topjava.service;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest  {
+
+    static {
+        // Only for postgres driver logging
+        // It uses java.util.logging and logged via jul-to-slf4j bridge
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService mealService;
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testGetBetweenInclusive() {
+    }
+
+    @Test
+    public void testGetAll() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testCreate() {
+    }
+}
